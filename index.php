4ab3afc577e094f7d4eb02777e5e0ceda53c3eca
prazny <biumrker@gmail.com>
2017-05-18 17:13:12 +0200

    composer

---
+++ index.php
@@ -0,0 +1,5 @@
+﻿<?php
+require 'vendor/autoload.php';
+
+$c = new \MyApp\Writing\Writing;
+echo $c;
---
+++ src/Writing/Write.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: PC
+ * Date: 16.05.2017
+ * Time: 15:27
+ */
+namespace MyApp\Writing;
+
+
+class Writing
+{
+    public function write (){
+        echo('ggg');
+    }
+    static function say(){
+        return "gggg";
+    }
+}
